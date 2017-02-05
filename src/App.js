4ef71d19c7b5ee9e76a023c4a183b3dd840c06a8
Dan Cabaniss <dccabs@gmail.com>
2017-02-05 16:20:51 -0600

    http request function for one earliest publication number

--- src/App.js
+++ src/App.js
@@ -12,28 +12,7 @@ const App = (props) => {
   return (
     <MuiThemeProvider>
       <AppShell>
-        <div style={{'padding': '20px 10%'}}>
-          <Card>
-            <CardTitle title="Search codes" />
-            <CardText>
-              In the textbox below, put up to 20 patent codes, one per line.  Then hit submit to get the details.
-              <form>
-                <TextField
-                  style={{width: '100%'}}
-                  floatingLabelText="Patent Codes"
-                  multiLine
-                  hintText="Paste your patent codes here, 1 code per line"
-                  />
-                  <RaisedButton
-                    style={{marginTop: '30px'}}
-                    label="Submit codes"
-                  />
-                </form>
-            </CardText>
-            <CardActions>
-            </CardActions>
-          </Card>
-        </div>
+        {props.children}
       </AppShell>
     </MuiThemeProvider>
   );
--- src/application/home/index.js
+++ src/application/home/index.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
 import App from '../../App'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 const Home = (props) => {
+  const handleSubmit = () => {
+    const xhr = window.fetch('https://pairbulkdata.uspto.gov/api/queries', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        df: 'patentTitle',
+        facet: true,
+        fl: '*',
+        fq: [],
+        mm: '100%',
+        qf: 'appEarlyPubNumber applId appLocation appType appStatus_txt appConfrNumber appCustNumber appGrpArtNumber appCls appSubCls appEntityStatus_txt patentNumber patentTitle primaryInventor firstNamedApplicant appExamName appExamPrefrdName appAttrDockNumber appPCTNumber appIntlPubNumber wipoEarlyPubNumber pctAppType firstInventorFile appClsSubCls rankAndInventorsList',
+        searchText: 'appEarlyPubNumber:(2017-0021088)',
+        sort: 'applId asc',
+        start: '0'
+      }),
+    }).then(response => {
+      return response.json()
+    }).then(responseData => {
+      return responseData.queryResults.searchResponse.response
+    }).then(data => {
+      console.log(data)
+    })
+  }
   return (
-    <App>
-      hello world
-    </App>
+    <div style={{'padding': '20px 10%'}}>
+      <Card>
+        <CardTitle title="Search codes" />
+        <CardText>
+          In the textbox below, put up to 20 patent codes, one per line.  Then hit submit to get the details.
+          <form onSubmit={handleSubmit}>
+            <TextField
+              style={{width: '100%'}}
+              floatingLabelText="Patent Codes"
+              multiLine
+              hintText="1 code per line"
+            />
+            <RaisedButton
+              style={{marginTop: '30px'}}
+              label="Submit codes"
+              type="submit"
+            />
+          </form>
+        </CardText>
+      </Card>
+
+      <div>
+
+      </div>
+    </div>
   );
 }
 
