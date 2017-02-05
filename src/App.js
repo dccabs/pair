ecb9ae97c2a777453e600e230bd83b0890c0cab3
Dan Cabaniss <dccabs@gmail.com>
2017-02-04 18:08:24 -0600

    progress

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppShell from './components/app_shell'
+import AppBar from 'material-ui/AppBar';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 const App = (props) => {
   return (
     <MuiThemeProvider>
-      <AppShell>first page</AppShell>
+      <AppShell>
+        <Card>
+          <CardTitle title="Card title" subtitle="Card subtitle" />
+          <CardText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+          <CardActions>
+            alsd;kfjasdf
+          </CardActions>
+        </Card>
+      </AppShell>
     </MuiThemeProvider>
   );
 }
---
+++ src/application/home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import App from '../../app'
+
+const Home = (props) => {
+  return (
+    <App>
+      hello world
+    </App>
+  );
+}
+
+export default App;
